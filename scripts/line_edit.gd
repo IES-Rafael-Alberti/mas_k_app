a7258e0a7b438a752526b978c784db8ff432b2a8
fjortegan <fjortegan@gmail.com>
2026-01-31 02:57:38 +0100

    masked text initial

---
+++ scripts/line_edit.gd
@@ -0,0 +1,11 @@
+extends LineEdit
+
+var container_margin = 10.0
+
+func _on_resized() -> void:
+	var line_edit = get_viewport().gui_get_focus_owner()
+	if line_edit:
+		var container = line_edit.get_parent()
+		if container.size.x - line_edit.size.x < container_margin:
+			line_edit.max_length = line_edit.text.length()
+			line_edit.caret_column = line_edit.max_length
